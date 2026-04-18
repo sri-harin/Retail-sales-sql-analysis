54af87c67db228d4dc3389f56ac85736517fbc36
SRIHARINI <sriharini3491@gmail.com>
2026-04-18 20:56:44 +0530

    Add files via upload

--- sql/data_cleaning.sql
+++ sql/data_cleaning.sql
@@ -1,1 +1,2 @@
-
+SELECT * FROM retail_sales WHERE sale_date IS NULL;
+DELETE FROM retail_sales WHERE sale_date IS NULL;
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,4 @@
+CREATE DATABASE p1_retail_db;
+USE p1_retail_db;
 
+CREATE TABLE retail_sales (...);
